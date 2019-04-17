04043ae821f9148df457c5ad3255dd6e58604f59
zanzan13531 <zanzan13531@gmail.com>
2019-04-16 22:57:57 -0700

    initial commit

---
+++ src/alt_tabber/Main.java
@@ -0,0 +1,11 @@
+package alt_tabber;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+		
+		tabber t = new tabber();
+	
+	}
+
+}
---
+++ src/alt_tabber/tabber.java
@@ -0,0 +1,11 @@
+package alt_tabber;
+
+public class tabber {
+	
+	public static void main(String[] args){
+		
+		
+		
+	}
+	
+}
